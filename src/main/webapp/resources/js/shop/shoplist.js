238c1f3aceae1709516df4db8da469adcd914775
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-09-21 21:35:29 +0800

    第六章：店铺编辑和列表功能完成

---
+++ src/main/webapp/resources/js/shop/shoplist.js
@@ -0,0 +1,58 @@
+$(function() {
+
+	// 调用，加载数据
+	getshoplist();
+
+	function getshoplist() {
+		$.ajax({
+			url : "/o2o/shop/getshoplist",
+			type : "get",
+			dataType : "json",
+			success : function(data) {
+				if (data.success) {
+					handleList(data.shopList);
+					handleUser(data.user);
+				}
+			}
+		});
+	}
+
+	// 显示名字
+	function handleUser(data) {
+		$('#user-name').text(data.name);
+	}
+
+	// 拼接店铺
+	function handleList(data) {
+		var shopListHtml = '';
+		data.map(function(item, index) {
+			shopListHtml += '<div class="row row-shop"><div class="col-40">'
+					+ item.shopName + '</div><div class="col-40">'
+					+ shopStatus(item.enableStatus)
+					+ '</div><div class="col-20">'
+					+ goShop(item.enableStatus, item.shopId) + '</div></div>'
+		});
+		$('.shop-wrap').html(shopListHtml);
+	}
+
+	function shopStatus(status) {
+		if (status == 0) {
+			return '审核中';
+		} else if (status == 1) {
+			return '审核通过';
+		} else {
+			return '店铺非法';
+		}
+	}
+
+	// 进入到商铺的管理页面,请求/shopadmin/shopmanagement ，进入到管理页面
+	function goShop(status, shopId) {
+		if (status == 1) {
+			return '<a href="/o2o/shopadmin/shopmanagement?shopId=' + shopId
+					+ '">进入</a>';
+		} else {
+			return '';
+		}
+	}
+
+});
---
+++ src/main/webapp/resources/js/shop/shopmanagement.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    // 获取shopId
+    var shopId = getQueryString("shopId");
+    // 商铺管理的url
+    var shopInfoUrl = '/o2o/shop/getshopmanageInfo?shopId=' + shopId;
+
+    $.getJSON(shopInfoUrl,function (data) {
+        // 如果后台返回redirect=true,则跳转后台到设置的url
+        if(data.redirect){
+            window.location.href = data.url;
+        }else{
+            // 如果后台返回redirect=false，则设置shopId并给 按钮设置超链接属性（即编辑商铺）
+            if (data.shopId != undefined && data.shopId != null){
+                shopId = data.shopId;
+            }
+            $('#shopInfo').attr('href','/o2o/shopadmin/shopoperation?shopId=' + shopId);
+            $('#productCategory').attr('href','/o2o/shopadmin/productcategorymanage?shopId=' + shopId);
+        }
+    });
+
+
+});
