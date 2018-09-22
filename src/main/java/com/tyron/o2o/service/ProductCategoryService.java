a43e5f029027b5579e8ee13fd0362fab5ffed0b7
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-09-22 22:48:34 +0800

    新增batchAddProductCategory方法

--- src/main/java/com/tyron/o2o/service/ProductCategoryService.java
+++ src/main/java/com/tyron/o2o/service/ProductCategoryService.java
@@ -5,7 +5,9 @@ package com.tyron.o2o.service;
 
 import java.util.List;
 
+import com.tyron.o2o.dto.ProductCategoryExecution;
 import com.tyron.o2o.entity.ProductCategory;
+import com.tyron.o2o.exceptions.ProductCategoryOperationException;
 
 /**
  * @Description: 店铺的商品类别业务接口
@@ -23,4 +25,13 @@ public interface ProductCategoryService {
 	 * @return
 	 */
 	List<ProductCategory> getProductCategoryList(Long shopId);
+
+	/**
+	 * 批量添加商品类别
+	 * 
+	 * @param productCategoryList
+	 * @return
+	 */
+	ProductCategoryExecution batchAddProductCategory(List<ProductCategory> productCategoryList)
+			throws ProductCategoryOperationException;
 }
--- src/main/java/com/tyron/o2o/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/tyron/o2o/service/impl/ProductCategoryServiceImpl.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tyron.o2o.dao.ProductCategoryDao;
+import com.tyron.o2o.dto.ProductCategoryExecution;
 import com.tyron.o2o.entity.ProductCategory;
+import com.tyron.o2o.enums.ProductCategoryStateEnum;
+import com.tyron.o2o.exceptions.ProductCategoryOperationException;
 import com.tyron.o2o.service.ProductCategoryService;
 
 /**
@@ -35,6 +39,35 @@ public class ProductCategoryServiceImpl implements ProductCategoryService {
 	@Override
 	public List<ProductCategory> getProductCategoryList(Long shopId) {
 		return productCategoryDao.queryProductCategoryList(shopId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.tyron.o2o.service.ProductCategoryService#batchAddProductCategory(java.
+	 * util.List)
+	 */
+	@Override
+	@Transactional
+	public ProductCategoryExecution batchAddProductCategory(List<ProductCategory> productCategoryList)
+			throws ProductCategoryOperationException {
+		// 列表不为空
+		if (productCategoryList != null && !productCategoryList.isEmpty()) {
+			try {
+				int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+				if (effectedNum <= 0) {
+					throw new ProductCategoryOperationException("店铺类别创建失败");
+				} else {
+					return new ProductCategoryExecution(ProductCategoryStateEnum.SUCCESS, productCategoryList,
+							effectedNum);
+				}
+			} catch (Exception e) {
+				throw new ProductCategoryOperationException("batchAddProductCategory error" + e.getMessage());
+			}
+		} else {
+			return new ProductCategoryExecution(ProductCategoryStateEnum.EMPETY_LIST);
+		}
 	}
 
 }
