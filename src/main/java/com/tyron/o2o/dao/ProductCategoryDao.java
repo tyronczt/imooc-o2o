4c67303785f14813c1b2bb33890be4e159f66447
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-09-22 22:51:48 +0800

    新增批量添加商品类别方法

--- src/main/java/com/tyron/o2o/dao/ProductCategoryDao.java
+++ src/main/java/com/tyron/o2o/dao/ProductCategoryDao.java
@@ -22,4 +22,12 @@ public interface ProductCategoryDao {
 	 * @return
 	 */
 	List<ProductCategory> queryProductCategoryList(long shopId);
+
+	/**
+	 * 批量添加商品类别
+	 * 
+	 * @param productCategoryList 商品类别列表
+	 * @return
+	 */
+	int batchInsertProductCategory(List<ProductCategory> productCategoryList);
 }
