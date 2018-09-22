d713becae4a214c0de52cbd81cde62a5583650c7
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-09-22 11:41:45 +0800

    修改

--- src/main/webapp/resources/js/shop/shopmanagement.js
+++ src/main/webapp/resources/js/shop/shopmanagement.js
@@ -15,7 +15,7 @@ $(function(){
                 shopId = data.shopId;
             }
             $('#shopInfo').attr('href','/o2o/shopadmin/shopoperation?shopId=' + shopId);
-            $('#productCategory').attr('href','/o2o/shopadmin/productcategorymanage?shopId=' + shopId);
+            $('#productCategory').attr('href','/o2o/shopadmin/productcategorymanagement');
         }
     });
 
