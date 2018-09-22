1c29250c7e6f6a0a03be190a851d91c02d18194f
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-09-22 11:41:33 +0800

    添加注释

--- src/main/java/com/tyron/o2o/util/PageCalculator.java
+++ src/main/java/com/tyron/o2o/util/PageCalculator.java
@@ -8,6 +8,13 @@ package com.tyron.o2o.util;
  */
 public class PageCalculator {
 
+	/**
+	 * 根据页数和每页数量查询行数
+	 * 
+	 * @param pageIndex 页码数
+	 * @param pageSize 每页数量
+	 * @return
+	 */
 	public static int calculateRowIndex(int pageIndex, int pageSize) {
 		return (pageIndex > 0) ? (pageIndex - 1) * pageSize : 0;
 	}
