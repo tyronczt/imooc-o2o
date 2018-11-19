1831e6fa1a7b46fcd1a801b99d5bdcac4f8df01e
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-11-20 01:06:53 +0800

    商品修改service测试类

--- src/test/java/com/tyron/o2o/service/ProductServiceTest.java
+++ src/test/java/com/tyron/o2o/service/ProductServiceTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,6 +31,7 @@ public class ProductServiceTest extends BaseTest {
 	private ProductService productService;
 
 	@Test
+	@Ignore
 	public void testAddShop() throws IOException {
 		Product product = new Product();
 		Shop shop = new Shop();
@@ -53,6 +55,36 @@ public class ProductServiceTest extends BaseTest {
 		MultipartFile productImg2 = ImageUtil.path2MultipartFile(filePath2);
 		productImgList.add(productImg2);
 		ProductExecution se = productService.addProduct(product, ImageUtil.path2MultipartFile(filePath0),
+				productImgList);
+		System.out.println("ProductExecution.state" + se.getState());
+		System.out.println("ProductExecution.stateInfo" + se.getStateInfo());
+	}
+
+	@Test
+	public void testModifyShop() throws IOException {
+		Product product = new Product();
+		Shop shop = new Shop();
+		shop.setShopId(1L);
+		product.setShop(shop);
+		ProductCategory productCategory = new ProductCategory();
+		productCategory.setProductCategoryId(2L);
+		product.setProductId(3L);
+		product.setProductCategory(productCategory);
+		product.setProductName("测试商品2");
+		product.setProductDesc("测试商品2描述");
+		product.setPriority(22);
+		product.setEnableStatus(ProductStateEnum.SUCCESS.getState());
+		product.setLastEditTime(new Date());
+		product.setCreateTime(new Date());
+		String filePath0 = "D:\\eclipse\\pic\\1.JPG";
+		List<MultipartFile> productImgList = new ArrayList<>();
+		String filePath1 = "D:\\eclipse\\pic\\2.JPG";
+		MultipartFile productImg1 = ImageUtil.path2MultipartFile(filePath1);
+		productImgList.add(productImg1);
+		String filePath2 = "D:\\eclipse\\pic\\3.JPG";
+		MultipartFile productImg2 = ImageUtil.path2MultipartFile(filePath2);
+		productImgList.add(productImg2);
+		ProductExecution se = productService.modifyProduct(product, ImageUtil.path2MultipartFile(filePath0),
 				productImgList);
 		System.out.println("ProductExecution.state" + se.getState());
 		System.out.println("ProductExecution.stateInfo" + se.getStateInfo());
