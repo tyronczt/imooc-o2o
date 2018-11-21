3db787d0e60b33b6b9a39e05e9272ce0b19d142d
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-11-22 01:10:07 +0800

    dao层新增分页查询方法

--- src/main/java/com/tyron/o2o/dao/ProductDao.java
+++ src/main/java/com/tyron/o2o/dao/ProductDao.java
@@ -1,5 +1,9 @@
 package com.tyron.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.tyron.o2o.entity.Product;
 
 /**
@@ -10,6 +14,25 @@ import com.tyron.o2o.entity.Product;
  */
 
 public interface ProductDao {
+
+	/**
+	 * 查询商品列表并分页，输入条件：商品名（模糊），商品状态，店铺Id，商品类别
+	 * 
+	 * @param productCondition 查询条件
+	 * @param rowIndex        行数
+	 * @param pageSize        每页数
+	 * @return
+	 */
+	List<Product> queryProductList(@Param("productCondition") Product productCondition, @Param("rowIndex") int rowIndex,
+			@Param("pageSize") int pageSize);
+
+	/**
+	 * 根据条件查询商品总数
+	 * 
+	 * @param productCondition 查询条件
+	 * @return
+	 */
+	int queryProductCount(@Param("productCondition") Product productCondition);
 
 	/**
 	 * 插入商品
