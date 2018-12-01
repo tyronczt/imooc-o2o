6a37945a480978fcf7bb367d813c94487477367c
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-12-01 23:28:10 +0800

    增加字段

--- src/main/java/com/tyron/o2o/enums/OperationStatusEnum.java
+++ src/main/java/com/tyron/o2o/enums/OperationStatusEnum.java
@@ -9,7 +9,7 @@ package com.tyron.o2o.enums;
 public enum OperationStatusEnum {
 
 	SUCCESS(1, "操作成功"), ERROR(-1001, "操作失败"), VERIFYCODE_ERROR(-1002, "验证码输入有误，请重新输入"), PIC_EMPTY(-1003, "上传图片不能为空"),
-	PIC_UPLOAD_ERROR(-1004, "创建图片失败");
+	PIC_UPLOAD_ERROR(-1004, "创建图片失败"), PAGIN_EMPTY(-1005, "缺少分页参数");
 
 	private int state;
 	private String stateInfo;
