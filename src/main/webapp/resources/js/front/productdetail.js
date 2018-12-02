6e87c1ac140dd37d8be3185b6a77bb79ff8fd4a3
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-12-03 01:18:40 +0800

    商品详情页面

--- src/main/webapp/resources/js/front/productdetail.js
+++ src/main/webapp/resources/js/front/productdetail.js
@@ -10,15 +10,24 @@ $(function() {
 				$('#product-time').text(new Date(product.lastEditTime) .Format("yyyy-MM-dd"));
 				$('#product-name').text(product.productName);
 				$('#product-desc').text(product.productDesc);
-				var imgListHtml = '';
-				product.productImgList.map(function(item, index) {
-					imgListHtml += '<div> <img src="'
-							+ item.imgAddr + '"/></div>';
+				// 获取商品详情图片列表
+				var productDetailImgList = product.productImgList;
+				var swiperHtml = '';
+				productDetailImgList.map(function(item, index) {
+					swiperHtml += ''
+                        + '<div class="swiper-slide img-wrap">'
+                        +      '<img class="banner-img" src="'+ item.piImgAddr +'" alt="'+ item.piImgDesc +'">'
+                        + '</div>';
 				});
 				// 生成购买商品的二维码供商家扫描
-				imgListHtml += '<div> <img src="/myo2o/front/generateqrcode4product?productId='
-						+ product.productId + '"/></div>';
-				$('#imgList').html(imgListHtml);
+//				imgListHtml += '<div> <img src="/o2o/front/generateqrcode4product?productId=' + product.productId + '"/></div>';
+				$('.swiper-wrapper').html(swiperHtml);
+				 // 设置轮播图轮换时间为1秒
+	            $(".swiper-container").swiper({
+	                autoplay: 1000,
+	                // 用户对轮播图进行操作时，是否自动停止autoplay
+	                autoplayDisableOnInteraction: true
+	            });
 			}
 		});
 	$('#me').click(function() {
