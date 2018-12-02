364fc04789b89b443253c3a7c5a5d5b27fde3895
Administrator <Administrator@Z8AL85J65W4BGSV>
2018-12-02 23:57:55 +0800

    商品详情控制器

---
+++ src/main/java/com/tyron/o2o/web/front/ProductDetailController.java
@@ -0,0 +1,54 @@
+package com.tyron.o2o.web.front;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tyron.o2o.entity.Product;
+import com.tyron.o2o.enums.ProductStateEnum;
+import com.tyron.o2o.service.ProductService;
+import com.tyron.o2o.util.HttpServletRequestUtil;
+
+/**
+ * @Description: 商品详情控制器
+ *
+ * @author tyronchen
+ * @date 2018年12月2日
+ */
+@Controller
+@RequestMapping("/front")
+public class ProductDetailController {
+
+	@Autowired
+	private ProductService productService;
+
+	/**
+	 * 获取商品详情
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/listproductdetailpageinfo", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> listProductDetailPageInfo(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		long productId = HttpServletRequestUtil.getLong(request, "productId");
+		Product product = null;
+		if (productId != -1) {
+			product = productService.getProductById(productId);
+			modelMap.put("product", product);
+			modelMap.put("success", true);
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", ProductStateEnum.PRODUCT_ID_EMPTY.getStateInfo());
+		}
+		return modelMap;
+	}
+}
