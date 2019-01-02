7502609eadd14aca272cbc142321104f923be41c
tyronczt <310674619@qq.com>
2019-01-02 23:31:28 +0800

    新增redis配置项

--- src/test/java/com/tyron/o2o/BaseTest.java
+++ src/test/java/com/tyron/o2o/BaseTest.java
@@ -5,14 +5,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * @Description: 测试基类
- *	配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
+ * @Description: 测试基类 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
  *
  * @author tyronchen
  * @date 2018年3月24日
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml" })
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml",
+		"classpath:spring/spring-redis.xml" })
 public class BaseTest {
 
 }
