61c9ac9884f5818f3110d835f752567840849d4d
tyronczt <310674619@qq.com>
2019-01-24 23:41:55 +0800

    头条异常处理

---
+++ src/main/java/com/tyron/o2o/exceptions/HeadLineException.java
@@ -0,0 +1,17 @@
+package com.tyron.o2o.exceptions;
+
+/**
+ * @Description: 头条操作异常
+ *
+ * @author tyronchen
+ * @date 2019年1月24日
+ */
+public class HeadLineException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public HeadLineException(String msg) {
+		super(msg);
+	}
+
+}
