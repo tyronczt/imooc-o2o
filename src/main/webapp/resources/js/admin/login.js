3c48e1c3e1cdce3c7f30dcaa52856db9aebbc3e4
tyronczt <310674619@qq.com>
2019-06-09 23:10:39 +0800

    add 用户登录页面

---
+++ src/main/webapp/resources/js/admin/login.js
@@ -0,0 +1,48 @@
+$(function() {
+	var loginUrl = '/o2o/user/login';
+	var loginCount = 0;
+
+	$('#submit').click(function() {
+		var username = $('#username').val();
+		var password = $('#psw').val();
+		var verifyCodeActual = $('#j_captcha').val();
+		var needVerify = false;
+		if (loginCount >= 3) {
+			if (!verifyCodeActual) {
+				$.toast('请输入验证码！');
+				return;
+			} else {
+				needVerify = true;
+			}
+		}
+		$.ajax({
+			url : loginUrl,
+			async : false,
+			cache : false,
+			type : "post",
+			dataType : 'json',
+			data : {
+				username : username,
+				password : password,
+				verifyCodeActual : verifyCodeActual,
+				needVerify : needVerify
+			},
+			success : function(data) {
+				if (data.success) {
+					$.toast('登录成功！');
+					window.location.href = '/o2o/shopadmin/shoplist';
+				} else {
+					$.toast(data.errMsg);
+					loginCount++;
+					if (loginCount >= 3) {
+						$('#verifyPart').show();
+					}
+				}
+			}
+		});
+	});
+
+	$('#register').click(function() {
+		window.location.href = '/o2o/admin/register';
+	});
+});
