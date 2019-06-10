03ceed1e0ef4f7ddd5c3f5ffe8c396a8413ad0ca
tyronczt <310674619@qq.com>
2019-06-11 00:39:46 +0800

    修改密码页面

---
+++ src/main/webapp/resources/js/admin/changepwd.js
@@ -0,0 +1,53 @@
+$(function() {
+	var url = '/o2o/user/changepwd';
+	$('#submit').click(function() {
+		var username = $('#username').val();
+		var password = $('#password').val();
+		var newPassword = $('#newPassword').val();
+		var renewpwd = $('#renewPassword').val();
+		// 新密码和确认密码不相同
+		if (newPassword != renewpwd) {
+			$.toast('两次密码输入不相同，请重新输入');
+			return;
+		}
+		// 新密码和原密码相同
+		if (newPassword == password) {
+			$.toast('新密码和原密码相同，请重新输入');
+			return;
+		}
+		var formData = new FormData();
+		formData.append('username', username);
+		formData.append('password', password);
+		formData.append('newPassword', newPassword);
+		var verifyCodeActual = $('#j_captcha').val();
+		if (!verifyCodeActual) {
+			$.toast('请输入验证码！');
+			return;
+		}
+		formData.append("verifyCodeActual", verifyCodeActual);
+		$.ajax({
+			url : url,
+			type : 'POST',
+			data : formData,
+			contentType : false,
+			processData : false,
+			cache : false,
+			success : function(data) {
+				if (data.success) {
+					$.toast('密码修改成功！');
+					// 延时2秒
+					setTimeout(function () {
+						window.location.href = '/o2o/admin/login';
+					}, 2000);
+				} else {
+					$.toast(data.errMsg);
+					$('#captcha_img').click();
+				}
+			}
+		});
+	});
+
+	$('#back').click(function() {
+		window.location.href = '/o2o/shopadmin/shoplist';
+	});
+});
