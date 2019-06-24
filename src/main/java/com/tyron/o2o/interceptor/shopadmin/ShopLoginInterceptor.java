9386028961bb21c38ff4f5de5608b4988057fc93
tyronczt <310674619@qq.com>
2019-06-24 23:28:04 +0800

    modify 店家管理系统登录拦截器

--- src/main/java/com/tyron/o2o/interceptor/shopadmin/ShopLoginInterceptor.java
+++ src/main/java/com/tyron/o2o/interceptor/shopadmin/ShopLoginInterceptor.java
@@ -1,7 +1,5 @@
 package com.tyron.o2o.interceptor.shopadmin;
 
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +7,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.tyron.o2o.entity.PersonInfo;
 import com.tyron.o2o.enums.EnableStatusEnum;
+import com.tyron.o2o.enums.PersonInfoTypeEnum;
 
 /**
  * @Description: 店家管理系统登录拦截器
@@ -27,27 +26,25 @@ public class ShopLoginInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		System.out.println(">>>ShopLoginInterceptor>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
 		// 从session中获取用户信息
 		Object userObj = request.getSession().getAttribute("user");
 		// 如果用户信息存在
 		if (userObj != null) {
 			// 将session中的用户信息转换为PersonInfo实体类对象
 			PersonInfo user = (PersonInfo) userObj;
-			// 用户存在且可用，并且用户类型为店家
+			// 用户存在且可用，并且用户类型为店家或者管理员
 			if (user != null && user.getUserId() != null && user.getUserId() > 0
-					&& user.getEnableStatus().equals(EnableStatusEnum.AVAILABLE.getState())) {
+					&& user.getEnableStatus().equals(EnableStatusEnum.AVAILABLE.getState())
+					&& (user.getUserType().equals(PersonInfoTypeEnum.OWNER.getState())
+							|| user.getUserType().equals(PersonInfoTypeEnum.ADMIN.getState()))) {
 				// 如果通过验证，则返回true，用户正常执行后续操作
 				return true;
 			}
 		}
-
-		// 不满足登录条件，则直接跳转用户登录页面
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<script>");
-		out.println("window.open('" + request.getContextPath() + "/local/login?usertype=2','_self)");
-		out.println("</script>");
-		out.println("</html>");
+		// 不满足登录条件，则直接跳转后台用户登录页面
+		String loginUrl = request.getContextPath() + "/admin/login?userType=back";
+		response.sendRedirect(loginUrl);
 		return false;
 	}
 
